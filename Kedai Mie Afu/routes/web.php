8f603ecf93364b5d447d51f61773fcf79c5cb80e
Mammuasyir <mammuasyir0101@gmail.com>
2021-10-01 22:59:58 +0700

    Add fitur SignIn & Signup

--- Kedai Mie Afu/routes/web.php
+++ Kedai Mie Afu/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::resource('test', TestController::class);
+Route::get('/','App\Http\Controllers\TestController@welcome')->name('welcome');
+// / detail > front detail -> name(detail) untuk Manggil di route(detail)
+Route::get('/detail','App\Http\Controllers\TestController@detail')->name('detail');
+// / keranjang > front keranjang -> name(keranjang) untuk Manggil di route(keranjang)
+Route::get('/bucket','App\Http\Controllers\TestController@bucket')->name('bucket');
+// / checkout > front checkou -> name(checkout) untuk Manggil di route(checkout)
+Route::get('/checkout','App\Http\Controllers\TestController@checkout')->name('checkout');
+
+Route::get('/signin','App\Http\Controllers\TestController@signin')->name('signin');
+Route::get('/signup','App\Http\Controllers\TestController@signup')->name('signup');
