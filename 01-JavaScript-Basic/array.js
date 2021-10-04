8c665c04f113ca3ce0442388e47658186d46cd40
Dimas Fendriansyah <dimasfendriansyah123098@gmail.com>
2021-10-04 21:12:37 +0700

    Array

---
+++ 01-JavaScript-Basic/array.js
@@ -0,0 +1,17 @@
+// Array
+
+// Sam dengan variabel yaitu menyimpan data tapi array
+// bisa menyimpan banyak data atau kumpulan data
+
+var array = ["Dimas","Dimus","Dimes"]
+
+var satu = array[1];
+
+console.log(satu);
+
+var mobil = ["toyota","suzuki"]
+
+mobil[0] = "dIMASta"
+console.log(mobil[0]);
+
+console.log(array.length);
--- 01-JavaScript-Basic/variabel.js
+++ 01-JavaScript-Basic/variabel.js
@@ -9,18 +9,31 @@
 // Contoh
 
 var hewan = "Ayam";
+console.log(hewan)
 
 var makanan = "Ikan"
+console.log(makanan)
 
 var nasiPadang ="Tempe"
+console.log(nasiPadang)
 
 var angkaDua = 2
+console.log(angkaDua)
 
 var angkaTiga = 3
+console.log(angkaTiga)
+
+// Mengubah isi variabel
+
+var nama = "Fulan"
+nama = "Dimas"
+console.log(nama)
 
 // Bisa juga menyimpan data kosong
 
 var kosong = null
+console.log(hewan)
 
 var stringKosong = ""
+console.log(hewan)
 
