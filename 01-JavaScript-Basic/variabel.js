5c164a8f6b9882d0c7459baa8df1e45dcc643e88
Dimas Fendriansyah <dimasfendriansyah123098@gmail.com>
2021-10-04 20:59:19 +0700

    JavaScript

---
+++ 01-JavaScript-Basic/variabel.js
@@ -0,0 +1,26 @@
+// Belajar Javascript
+
+// Variabel
+
+// Variabel merupakan kumpulan data yang isinya bisa berupa
+// data tulisan atau angka
+// atau apapun yang kalian mau asalkan valid
+
+// Contoh
+
+var hewan = "Ayam";
+
+var makanan = "Ikan"
+
+var nasiPadang ="Tempe"
+
+var angkaDua = 2
+
+var angkaTiga = 3
+
+// Bisa juga menyimpan data kosong
+
+var kosong = null
+
+var stringKosong = ""
+
