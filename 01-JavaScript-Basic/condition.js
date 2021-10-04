ec2e52101a8d5d7e154046f64bb6b522838231b9
Dimas Fendriansyah <dimasfendriansyah123098@gmail.com>
2021-10-04 22:10:28 +0700

    conditions

---
+++ 01-JavaScript-Basic/condition.js
@@ -0,0 +1,18 @@
+
+
+var mood = 'ngantuk'
+
+
+if (mood == 'ngantuk') {
+    console.log("Tidur yok")
+}
+
+var grade = 90
+
+if (grade <= 60) {
+    console.log('Yah cupu C')
+} else if (grade >= 61 && grade <= 70) {
+    console.log('b aja B')
+} else if (grade >= 71) {
+    console.log('amat baik A')
+}
